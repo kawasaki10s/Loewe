73c7c3a5667520a60d976e408a669cc38c81fd72
ErkinovIbrohimjon <ibrohimlive10@gmail.com>
2025-11-17 11:18:46 +0500

    My bot servies Added

--- src/main/java/org/example/MyBotservies.java
+++ src/main/java/org/example/MyBotservies.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 public class MyBotservies {
+
+    public SendMessage Ozbemzayu(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("salom");
+
+        return sendMessage;
+    }
 }
