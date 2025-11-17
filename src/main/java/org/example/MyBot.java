ff28ddafd11001903e864cce3653156c2d7c5710
ErkinovIbrohimjon <ibrohimlive10@gmail.com>
2025-11-17 10:58:16 +0500

    First Open

---
+++ src/main/java/org/example/MyBot.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "@Looewe_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "8596659549:AAG_8g-QBiXRW6O9ZYnqVm9B8hqsLH7aoJM";
+    }
+}
