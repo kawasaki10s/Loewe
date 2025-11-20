2dc9022d22088e4690d4e43398e0eae16a8d7666
Sarvarbek <sarvarbek1545qadirberganov@gmail.com>
2025-11-20 09:38:30 +0500

    Added UzBaggyMenu

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -60,32 +60,13 @@ public class MyBot extends TelegramLongPollingBot {
                     throw new RuntimeException(e);
                 }
             }
-            if (data.equals("uzBaggyId")){
+            if (data.equals("uzBaggyId")) {
                 try {
                     execute(myBotservies.uzBaggyMenu(chatId));
                 } catch (TelegramApiException e) {
                     throw new RuntimeException(e);
                 }
             }
-            if (data.equals("uzBaggyHoodieId")){
-                try {
-                    execute(myBotservies.UzHoodiePicture1(chatId));
-//                    execute(myBotservies.UzHoodiePicture2(chatId));
-//                    execute(myBotservies.UzHoodiePicture3(chatId));
-//                    execute(myBotservies.UzHoodiePicture4(chatId));
-//                    execute(myBotservies.UzHoodiePicture5(chatId));
-//                    execute(myBotservies.UzHoodiePicture6(chatId));
-//                    execute(myBotservies.UzHoodiePicture7(chatId));
-//                    execute(myBotservies.UzHoodiePicture8(chatId));
-//                    execute(myBotservies.UzHoodiePicture9(chatId));
-//                    execute(myBotservies.UzHoodiePicture10(chatId));
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-
-            //
 
 
 
